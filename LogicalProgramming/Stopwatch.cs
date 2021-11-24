4049536627581e3eed41bbaad415edf3a0c9e6c0
faheemjailor <faheemjailor0055>
2021-11-24 18:56:35 +0530

    Faheem UC for stopwatch

---
+++ LogicalProgramming/Stopwatch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+
+namespace BasicLogicalProgramming
+{
+	class StopWatch
+	{
+		public void stopWatch()
+		{
+			// Create new stopwatch
+			Stopwatch stopwatch = new Stopwatch();
+
+			// Begin timing
+			stopwatch.Start();
+
+			// Do something
+			for (int i = 0; i < 1000; i++)
+			{
+				Thread.Sleep(1);
+			}
+
+			// Stop timing
+			stopwatch.Stop();
+
+			// result
+			Console.WriteLine("Time elapsed: {0}",
+				stopwatch.Elapsed);
+		}
+	}
+}
